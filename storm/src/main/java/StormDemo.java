020edd9a44a06b56188b2e12b9a35be940b9452b
yanbit <yankun@elex-tech.com>
2015-11-11 18:26:20 +0800

    test code

--- storm/src/main/java/StormDemo.java
+++ storm/src/main/java/StormDemo.java
@@ -47,6 +47,7 @@ public class StormDemo {
 
     Config conf = new Config();
 
+    //out
     // set producer properties.
     Properties props = new Properties();
     props.put("metadata.broker.list",
